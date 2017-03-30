a947bb4cb4eac0f791312b68b133a5981a0eb91c
Kevin Thrailkill <kevinjt92@gmail.com>
2017-03-30 12:11:19 -0700

    Have tab bar set up and tableviews set up with table view cells

---
+++ flicks/flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flicks
+//
+//  Created by Kevin Thrailkill on 3/30/17.
+//  Copyright © 2017 kevinthrailkill. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flicks/flicks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  flicks
+//
+//  Created by Kevin Thrailkill on 3/30/17.
+//  Copyright © 2017 kevinthrailkill. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
